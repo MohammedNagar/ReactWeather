04d066b946dad05f6902afcb5c218ebd52d4b56c
Anyname Donotcare <Anyname Donotcare>
2017-04-17 14:00:08 +0200

    Rename example page

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 var express = require('express');
 
-//Create Our application
+// Create our app
 var app = express();
 const PORT = process.env.PORT || 3000;
-app.use(function(req,res,next){
 
-  if(req.headers['x-forwarded-proto']==='http'){
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('http://' + req.hostname + req.url);
+  } else {
     next();
-  }else {
-    req.redirect('http://' + req.hostname + req.url);
   }
-
 });
 
 app.use(express.static('public'));
 
-app.listen(PORT,function(){
-   console.log('Express Server Is Up On Port '+ PORT);
+app.listen(PORT, function () {
+  console.log('Express server is up on port ' + PORT);
 });
+
+
+//http://stackoverflow.com/questions/3755529/git-remote-doesnt-seem-to-be-working-at-all
+//http://stackoverflow.com/questions/8786564/cannot-push-to-heroku-because-key-fingerprint
+//http://stackoverflow.com/questions/17846529/could-not-open-a-connection-to-your-authentication-agent
